252ec121e0442c1c2df6240d88115ce18e341f56
KaiTyrusNelson <45576196+KaiTyrusNelson@users.noreply.github.com>
2020-05-07 13:02:12 -0700

    Update noxfile.py

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,7 @@
 import shlex
 
 import nox
-@nox.session(python="3.6")
+@nox.session(python=["3.5", "3.6", "3.7", "3.8"])
 def tests(session):
 
 
@@ -14,11 +14,26 @@ def tests(session):
     session.install('pandas>=0.25')
     session.install('coverage')
     session.install('cython>=0.2')
-
     # build
     session.run(*shlex.split("python setup.py sdist"))
     session.run(*shlex.split("pip install dist/BARNI-1.0.0.tar.gz"))
-
+    #run tests
+    session.run(*shlex.split("python setup.py test"))
+    
+@nox.session(python="3.8")    
+def coverage(session):
+    # install requirements
+    session.install('pyyaml>=5.1')
+    session.install('numpy>=1.17')
+    session.install('scipy>=1.3.0')
+    session.install('scikit-learn>=0.20.0')
+    session.install('bokeh>=1.4.0')
+    session.install('pandas>=0.25')
+    session.install('coverage')
+    session.install('cython>=0.2')
+    # build
+    session.run(*shlex.split("python setup.py sdist"))
+    session.run(*shlex.split("pip install dist/BARNI-1.0.0.tar.gz"))
     #run coverage
     session.run(*shlex.split('coverage run -m unittest discover -v'))
     session.run(*shlex.split('coverage report --include="barni*" --omit="test*"'))

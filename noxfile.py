9f8f763cb4bca47e282bcb145c9e4bf8a7fce789
Kai Nelson <tyrusmcnelson@gmail.com>
2020-05-07 18:26:54 -0700

    XML OUTPUT FOR COVERAGE

--- noxfile.py
+++ noxfile.py
@@ -1,39 +1,42 @@
 import shlex
-
 import nox
+
+nox.options.sessions = ["report"]
+
 @nox.session(python=["3.5", "3.6", "3.7", "3.8"])
 def tests(session):
 
 
     # install requirements
-    session.install('pyyaml>=5.1')
-    session.install('numpy>=1.17')
-    session.install('scipy>=1.3.0')
-    session.install('scikit-learn>=0.20.0')
-    session.install('bokeh>=1.4.0')
-    session.install('pandas>=0.25')
-    session.install('coverage')
-    session.install('cython>=0.2')
+    session.install('-r', 'requirements.txt')
     # build
     session.run(*shlex.split("python setup.py sdist"))
     session.run(*shlex.split("pip install dist/BARNI-1.0.0.tar.gz"))
     #run tests
     session.run(*shlex.split("python setup.py test"))
     
-@nox.session(python="3.8")    
+@nox.session    
 def coverage(session):
     # install requirements
-    session.install('pyyaml>=5.1')
-    session.install('numpy>=1.17')
-    session.install('scipy>=1.3.0')
-    session.install('scikit-learn>=0.20.0')
-    session.install('bokeh>=1.4.0')
-    session.install('pandas>=0.25')
-    session.install('coverage')
-    session.install('cython>=0.2')
+    session.install('-r', 'requirements.txt')
+
     # build
     session.run(*shlex.split("python setup.py sdist"))
     session.run(*shlex.split("pip install dist/BARNI-1.0.0.tar.gz"))
+
+    #run coverage
+    session.run(*shlex.split('coverage run -m unittest discover -v'))
+    session.run(*shlex.split('coverage xml --include="barni*" --omit="test*"'))
+
+@nox.session    
+def report(session):
+    # install requirements
+    session.install('-r', 'requirements.txt')
+
+    # build
+    session.run(*shlex.split("python setup.py sdist"))
+    session.run(*shlex.split("pip install dist/BARNI-1.0.0.tar.gz"))
+
     #run coverage
     session.run(*shlex.split('coverage run -m unittest discover -v'))
     session.run(*shlex.split('coverage report --include="barni*" --omit="test*"'))
